4aef87727be8e5e8380d132303b90ce19d3a329f
s14002 <s14002@std.it-college.ac.jp>
2015-11-25 10:27:34 +0900

    Add Input.java

--- app/src/main/java/jp/ac/it_college/std/s14002/android/tetris/Board.java
+++ app/src/main/java/jp/ac/it_college/std/s14002/android/tetris/Board.java
@@ -5,13 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.util.ArrayList;
 
 /**
  * Created by s14002 on 15/11/11.
@@ -21,7 +21,9 @@ public class Board extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder holder;
     private DrawThread thread;
     private Bitmap blocks;
-    private Rect[] blockRectArray = new Rect[7];
+    private Tetromino fallingTetromino;
+    private ArrayList<Tetromino> tetrominoList = new ArrayList<>();
+    private long count = 0;
 
 
     public Board(Context context) {
@@ -40,11 +42,34 @@ public class Board extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     private void initialize(Context context) {
+        getHolder().addCallback(this);
         blocks = BitmapFactory.decodeResource(context.getResources(), R.drawable.block);
-        int side = blocks.getWidth(); // or getHeight()
-        for (int i = 0; i < blockRectArray.length; i++) {
-            blockRectArray[i] = new Rect(0, i * side, side, (i + 1)* side);
+        spawnTetromino();
+    }
+
+    public void spawnTetromino() {
+        fallingTetromino = new Tetromino(this);
+        fallingTetromino.setPosition(5, 23);
+    }
+
+    public boolean fallTetromino() {
+        fallingTetromino.move(Input.Down);
+        if (isVoidPosition()) {
+            fallingTetromino.move(Input.Up);
         }
+        return true;
+    }
+
+    public boolean isVoidPosition() {
+        boolean overlapping = false;
+        for (Tetromino fixedTetromino: tetrominoList) {
+            if (fallingTetromino.intersect(fixedTetromino)) {
+                overlapping = true;
+                break;
+            }
+        }
+
+        return !(overlapping || fallingTetromino.isOutOfBounds());
 
     }
 
@@ -56,47 +81,52 @@ public class Board extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         stopThread();
-
     }
-    private void draw() {
-        Canvas canvas = holder.lockCanvas();
+
+    public void translateCanvasCoordinate(Canvas canvas, RectF rectF, int gx, int gy) {
+        float side = canvas.getWidth() / 10.0f;
+        gy = 20 - gy;
+        rectF.set(side * gx, side * gy, side * (gx + 1), side * (gy + 1));
+    }
+
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
         if (canvas == null) {
             return;
         }
-        int width = canvas.getWidth();
-        int height = canvas.getHeight();
+
+        if (!Tetromino.Type.isBitmapinitialized()) {
+            Tetromino.Type.setBlockBitmap(blocks);
+
+        }
+        updateGame();
 
         canvas.drawColor(Color.LTGRAY); // 画面クリア（単色塗りつぶし）
-        float side = width / 10.0f;
-        RectF destRect = new RectF(0, 0, side, side);
-        Paint paint = new Paint();
 
-        destRect.offset(side, side);
-        canvas.drawBitmap(blocks, blockRectArray[0], destRect, paint);
+        for (Tetromino tetromino : tetrominoList) {
+            tetromino.draw(canvas);
+        }
+        fallingTetromino.draw(canvas);
+    }
 
-        destRect.offset(side, side);
-        canvas.drawBitmap(blocks, blockRectArray[1], destRect, paint);
+    public void send(Input input) {
 
-        destRect.offset(side, side);
-        canvas.drawBitmap(blocks, blockRectArray[2], destRect, paint);
+        fallingTetromino.move(input);
+    }
 
-        destRect.offset(side, side);
-        canvas.drawBitmap(blocks, blockRectArray[3], destRect, paint);
-
-        destRect.offset(side, side);
-        canvas.drawBitmap(blocks, blockRectArray[4], destRect, paint);
-
-        destRect.offset(side, side);
-        canvas.drawBitmap(blocks, blockRectArray[5], destRect, paint);
-
-        destRect.offset(side, side);
-        canvas.drawBitmap(blocks, blockRectArray[6], destRect, paint);
+    private void updateGame() {
+        if (count++ % (FPS / 2) != 0) {
+            return;
+        }
+        if (!fallTetromino()) {
+            tetrominoList.add(fallingTetromino);
+            spawnTetromino();
+        }
     }
 
     private void startThread() {
@@ -107,7 +137,7 @@ public class Board extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     private void stopThread() {
-        if (thread == null) {
+        if (thread != null) {
             thread.isFinished = true;
             thread = null;
         }
@@ -115,12 +145,14 @@ public class Board extends SurfaceView implements SurfaceHolder.Callback {
 
     private class DrawThread extends Thread {
         private boolean isFinished;
+
+
         @Override
         public void run() {
-            long preTime = System.currentTimeMillis();
+            long prevTime = 0;
             while (!isFinished) {
                 if (holder == null ||
-                        System.currentTimeMillis() - preTime < 1000 / FPS) {
+                        System.currentTimeMillis() - prevTime < 1000 / FPS) {
                     try {
                         sleep(1000 / FPS / 3);
                     } catch (InterruptedException e) {
@@ -140,7 +172,7 @@ public class Board extends SurfaceView implements SurfaceHolder.Callback {
                         holder.unlockCanvasAndPost(c);
                     }
                 }
-                preTime = System.currentTimeMillis();
+                prevTime = System.currentTimeMillis();
             }
         }
 
---
+++ app/src/main/java/jp/ac/it_college/std/s14002/android/tetris/Input.java
@@ -0,0 +1,8 @@
+package jp.ac.it_college.std.s14002.android.tetris;
+
+/**
+ * Created by s14002 on 15/11/25.
+ */
+public enum  Input {
+    Left, Right,Up, Down, Rotate;
+}
