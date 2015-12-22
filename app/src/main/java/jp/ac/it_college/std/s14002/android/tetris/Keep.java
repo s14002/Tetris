5663e5390b702ef7ceb355ecada696da80bf78d6
s14002 <s14002@std.it-college.ac.jp>
2015-12-22 23:14:25 +0900

    Update Keep.java

--- app/src/main/java/jp/ac/it_college/std/s14002/android/tetris/Keep.java
+++ app/src/main/java/jp/ac/it_college/std/s14002/android/tetris/Keep.java
@@ -1,6 +1,7 @@
 package jp.ac.it_college.std.s14002.android.tetris;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -8,15 +9,17 @@ import android.view.SurfaceView;
  * Created by s14002 on 15/11/11.
  */
 public class Keep extends SurfaceView implements SurfaceHolder.Callback {
-
+    private SurfaceHolder holder;
 
     public Keep(Context context) {
         super(context);
+        getHolder().addCallback(this);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         this.holder = holder;
+        statThread();
     }
 
     @Override
@@ -32,14 +35,20 @@ public class Keep extends SurfaceView implements SurfaceHolder.Callback {
     private void statThread() {
         stopThread();
 
-        thread = new DrawThread();
-        thread.start();
+       /* thread = new DrawThread();
+        thread.start();*/
     }
 
     private void stopThread() {
-        if (thread != null) {
+      /*  if (thread != null) {
             thread.isFinished = true;
             thread = null;
-        }
+        }*/
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        
     }
 }
